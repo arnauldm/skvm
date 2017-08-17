1fa266188339cbcca1499ccecb86c3c746e51597
Arnauld Michelizza <a.michelizza@gmail.com>
2017-08-17 18:03:14 +0200

    (minor)

--- skvm.c
+++ skvm.c
@@ -18,17 +18,17 @@
 
 /* CONFIGURATION BEGIN */
 
-#define RAM_SIZE (32*MBYTE)    /* Memory in bytes */
+#define RAM_SIZE (32*MBYTE)     /* Memory in bytes */
 #define KVM_FILE "/dev/kvm"     /* KVM special file */
 
 #define IVT_ADDR        0x00000000
-#define LOAD_ADDR       0x00001000  /* Guest */
+#define LOAD_ADDR       0x00001000      /* Guest */
 #define BIOS_ADDR       0x000f0000
-#define PCI_HOLE_ADDR   0xc0000000  /* PCI hole physical address */
+#define PCI_HOLE_ADDR   0xc0000000      /* PCI hole physical address */
 
 /* CONFIGURATION END */
 
-char *vm_ram;             /* Pointer to allocated RAM for the VM */
+char *vm_ram;                   /* Pointer to allocated RAM for the VM */
 
 void pexit (char *s)
 {
@@ -44,7 +44,7 @@ void *GPA_to_HVA (uint64_t offset)
 
 int main (int argc, char **argv)
 {
-    int guest_file_fd;               /* File descriptor to the guest image file in raw format */
+    int guest_file_fd;          /* File descriptor to the guest image file in raw format */
     int kvm_fd;                 /* Mainly file descriptor to "/dev/kvm" file */
     int vm_fd;                  /* File descriptor to the VM created by KVM API */
     int vcpu_fd;                /* File descriptor to VCPU */
@@ -101,14 +101,14 @@ int main (int argc, char **argv)
     /* Enabling in-kernel irqchip (PIC) and PIT */
 
     /*
-    ret = ioctl (vm_fd, KVM_CREATE_PIT2, &pit_config);
-    if (ret < 0)
-        pexit ("KVM_CREATE_PIT2 ioctl");
+       ret = ioctl (vm_fd, KVM_CREATE_PIT2, &pit_config);
+       if (ret < 0)
+       pexit ("KVM_CREATE_PIT2 ioctl");
 
-    ret = ioctl (vm_fd, KVM_CREATE_IRQCHIP);
-    if (ret < 0)
-        pexit ("KVM_CREATE_IRQCHIP ioctl");
-    */
+       ret = ioctl (vm_fd, KVM_CREATE_IRQCHIP);
+       if (ret < 0)
+       pexit ("KVM_CREATE_IRQCHIP ioctl");
+     */
 
     /* "This ioctl defines the physical address of a three-page region in the
      * guest physical address space.  The region must be within the first 4GB
@@ -123,7 +123,8 @@ int main (int argc, char **argv)
     /* Reserve and init RAM */
     /* mmap - MAP_NORESERVE to do not reserve swap space for this mapping */
     vm_ram =
-        mmap (NULL, RAM_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS | MAP_NORESERVE, -1, 0);
+        mmap (NULL, RAM_SIZE, PROT_READ | PROT_WRITE,
+              MAP_SHARED | MAP_ANONYMOUS | MAP_NORESERVE, -1, 0);
 
     if ((void *) vm_ram == MAP_FAILED)
         pexit ("mmap");
@@ -135,11 +136,11 @@ int main (int argc, char **argv)
 
     /* Allocating physical memory to the guest */
     region = (struct kvm_userspace_memory_region) {
-        .slot = 0,                  /* bits 0-15 of "slot" specifies the slot id */
+        .slot = 0, /* bits 0-15 of "slot" specifies the slot id */
         .flags = 0,
-        .guest_phys_addr = 0,       /* start of the VM physical memory */
-        .memory_size = RAM_SIZE,    /* bytes */
-        .userspace_addr = (uint64_t) vm_ram,  /* start of the userspace allocated memory */
+        .guest_phys_addr = 0, /* start of the VM physical memory */
+        .memory_size = RAM_SIZE, /* bytes */
+        .userspace_addr = (uint64_t) vm_ram, /* start of the userspace allocated memory */
     };
 
     ret = ioctl (vm_fd, KVM_SET_USER_MEMORY_REGION, &region);
@@ -161,32 +162,32 @@ int main (int argc, char **argv)
         pexit ("KVM_GET_VCPU_MMAP_SIZE ioctl");
 
     kvm_run =
-        mmap (NULL, (size_t) mmap_size, PROT_READ | PROT_WRITE, MAP_SHARED, vcpu_fd,
-              0);
+        mmap (NULL, (size_t) mmap_size, PROT_READ | PROT_WRITE, MAP_SHARED,
+              vcpu_fd, 0);
     if (kvm_run == MAP_FAILED)
         pexit ("unable to mmap vcpu fd");
 
 
     /* Settings guest registers */
-    ret = ioctl(vcpu_fd, KVM_GET_SREGS, &sregs);
+    ret = ioctl (vcpu_fd, KVM_GET_SREGS, &sregs);
     if (ret < 0)
         pexit ("KVM_GET_SREGS ioctl");
 
-    sregs.cs.base = 0; /* FIXME */
+    sregs.cs.base = 0;          /* FIXME */
     sregs.cs.selector = 0;
 
-    ret = ioctl(vcpu_fd, KVM_SET_SREGS, &sregs);
+    ret = ioctl (vcpu_fd, KVM_SET_SREGS, &sregs);
     if (ret < 0)
         pexit ("KVM_SET_SREGS ioctl");
 
-    ret = ioctl(vcpu_fd, KVM_GET_REGS, &regs);
+    ret = ioctl (vcpu_fd, KVM_GET_REGS, &regs);
     if (ret < 0)
         pexit ("KVM_GET_REGS ioctl");
 
     regs.rip = LOAD_ADDR;
-    regs.rflags = 2; /* bit 1 is reserved and must be set */
+    regs.rflags = 2;            /* bit 1 is reserved and must be set */
 
-    ret = ioctl(vcpu_fd, KVM_SET_REGS, &regs);
+    ret = ioctl (vcpu_fd, KVM_SET_REGS, &regs);
     if (ret < 0)
         pexit ("KVM_SET_REGS ioctl");
 
@@ -201,34 +202,38 @@ int main (int argc, char **argv)
 
 
     while (1) {
-        ret = ioctl(vcpu_fd, KVM_RUN, 0);
+        ret = ioctl (vcpu_fd, KVM_RUN, 0);
         if (ret < 0)
             pexit ("KVM_RUN ioctl");
-    
+
         switch (kvm_run->exit_reason) {
 
-            case KVM_EXIT_HLT:
-                fprintf (stderr, "guest halted\n");
-                return 0;
+        case KVM_EXIT_HLT:
+            fprintf (stderr, "guest halted\n");
+            return 0;
 
-            case KVM_EXIT_IO:
-                /* if output to default serial port COM0 */
-                if (kvm_run->io.port == 0x3f8 && kvm_run->io.direction == KVM_EXIT_IO_OUT) {
-                    write (STDERR_FILENO, (char*) (kvm_run) + kvm_run->io.data_offset, kvm_run->io.size);
-                }
-                else {
-                    fprintf (stderr, "unhandled KVM_EXIT_IO (port: 0x%x)\n", kvm_run->io.port);
-                }
-                break;
+        case KVM_EXIT_IO:
+            /* if output to default serial port COM0 */
+            if (kvm_run->io.port == 0x3f8
+                && kvm_run->io.direction == KVM_EXIT_IO_OUT) {
+                write (STDERR_FILENO,
+                       (char *) (kvm_run) + kvm_run->io.data_offset,
+                       kvm_run->io.size);
+            } else {
+                fprintf (stderr, "unhandled KVM_EXIT_IO (port: 0x%x)\n",
+                         kvm_run->io.port);
+            }
+            break;
 
-            case KVM_EXIT_FAIL_ENTRY:
-                fprintf (stderr, "KVM_EXIT_FAIL_ENTRY: fail_entry.hardware_entry_failure_reason: = 0x%llx\n",
-                    kvm_run->fail_entry.hardware_entry_failure_reason);
-                return 1;
+        case KVM_EXIT_FAIL_ENTRY:
+            fprintf (stderr,
+                     "KVM_EXIT_FAIL_ENTRY: fail_entry.hardware_entry_failure_reason: = 0x%llx\n",
+                     kvm_run->fail_entry.hardware_entry_failure_reason);
+            return 1;
 
-            default:
-                fprintf (stderr, "exit_reason = 0x%x\n", kvm_run->exit_reason);
-                return 1;
+        default:
+            fprintf (stderr, "exit_reason = 0x%x\n", kvm_run->exit_reason);
+            return 1;
         }
     }
 
