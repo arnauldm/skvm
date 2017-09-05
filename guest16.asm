79847cd5b4f6f65737d9a3697faa7deaef2f2f47
Arnauld Michelizza <a.michelizza@gmail.com>
2017-09-05 17:15:49 +0200

    added: pretty messages

--- guest16.asm
+++ guest16.asm
@@ -11,21 +11,35 @@ _start:
   mov ss, ax
   mov sp, 0x2000
 
-hello:
-  mov si, msg
-.start:
+  mov si, hello_msg
+  call display
+
+.halt:
+  mov si, last_msg
+  call display
+  hlt
+  jmp .halt
+
+
+display:
+  push ax
+  push bx
+.display_start:
   lodsb         ; ds:si -> al
   cmp al, 0     ; fin chaine ?
-  jz .end
+  jz .display_end
   mov ah, 0x0E  ; service 0Eh
   mov bx, 0x07  ; bx -> attribut 
   int 0x10
-  jmp .start
-.end:
-
-  hlt
+  jmp .display_start
+.display_end:
+  pop bx
+  pop ax
+  ret
 
 ;--- Variables ---
-msg db "Hello, world!", 10, 0
+hello_msg db "guest: Hello, world!", 10, 0
+last_msg db "guest: Goodbye", 10, 0
+test_msg db "guest: Test", 10, 0
 
 code16_end:
